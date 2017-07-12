859f1624ef6f4e852d616e428d5d0964946c1511
Evan Klitzke <evan@eklitzke.org>
2017-07-12 13:38:15 -0700

    appease shellcheck

--- backup-git.sh
+++ backup-git.sh
@@ -29,14 +29,14 @@ BUCKET="$1"
 renice -n 10 $$
 
 # Clean things up when we're done
-trap "rm -f ${TEMPDIR}/*.git.tar.xz" EXIT
+trap 'rm -f ${TEMPDIR}/*.git.tar.xz' EXIT
 
 cd $TEMPDIR
 
 # create tar archives of each git repo
 for proj in ${GITDIR}/*.git; do
-    base=$(basename $proj)
-    tar -C $GITDIR -cJf ${base}.tar.xz ${base}
+    base=$(basename "$proj")
+    tar -C $GITDIR -cJf "${base}.tar.xz" "${base}"
 done
 
 upload() {
@@ -48,10 +48,10 @@ upload() {
 # If hashes.txt exists, only upload files whose hashes have changed. Otherwise,
 # upload everything.
 if [ -f hashes.txt ]; then
-    upload $(sha1sum -c hashes.txt 2>/dev/null | awk -F: '/FAILED/ {print $1}')
+    upload "$(sha1sum -c hashes.txt 2>/dev/null | awk -F: '/FAILED/ {print $1}')"
 else
-    upload *.git.tar.xz
+    upload ./*.git.tar.xz
 fi
 
 # Write out the new file hashes for the next run.
-sha1sum *.git.tar.xz >hashes.txt
+sha1sum ./*.git.tar.xz >hashes.txt
